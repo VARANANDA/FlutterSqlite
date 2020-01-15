3b39d2c544500b739371559204f0e7d87ef6adde
varananda <varananda@varanandas-MacBook-Air.local>
2020-01-15 17:31:32 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,236 @@
+import 'package:flutter/material.dart';
+import 'employee.dart';
+import 'dart:async';
+import 'db_helper.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  Future<List<Employee>> employees;
+  TextEditingController controller = TextEditingController();
+  String name;
+  int curUserId;
+
+  final formKey = new GlobalKey<FormState>();
+  var dbHelper;
+  bool isUpdating;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    dbHelper = DBHelper();
+    isUpdating = false;
+    refreshList();
+  }
+
+  refreshList() {
+    setState(() {
+      employees = dbHelper.getEmployees();
+    });
+  }
+
+  clearName() {
+    controller.text = '';
+  }
+
+  validate() {
+    if (formKey.currentState.validate()) {
+      formKey.currentState.save();
+      if (isUpdating) {
+        Employee e = Employee(curUserId, name);
+        dbHelper.update(e);
+
+        setState(() {
+          isUpdating = false;
+        });
+      } else {
+        Employee e = Employee(null, name);
+        dbHelper.save(e);
+      }
+      clearName();
+      refreshList();
+    }
+  }
+
+  form() {
+    return Form(
+      key: formKey,
+      child: Padding(
+        padding: EdgeInsets.all(15.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          verticalDirection: VerticalDirection.down,
+          children: <Widget>[
+            TextFormField(
+              controller: controller,
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(labelText: 'Name'),
+              validator: (val) => val.length == 0 ? 'Enter Name' : null,
+              onSaved: (val) => name = val,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                FlatButton(
+                  onPressed: validate,
+                  child: Text(isUpdating ? 'UPDATE' : 'ADD'),
+                ),
+                FlatButton(
+                  onPressed: () {
+                    setState(() {
+                      isUpdating = false;
+                    });
+                    clearName();
+                  },
+                  child: Text('CANCEL'),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+//  void _incrementCounter() {
+//    setState(() {
+//      // This call to setState tells the Flutter framework that something has
+//      // changed in this State, which causes it to rerun the build method below
+//      // so that the display can reflect the updated values. If we changed
+//      // _counter without calling setState(), then the build method would not be
+//      // called again, and so nothing would appear to happen.
+//      _counter++;
+//    });
+//  }
+
+  SingleChildScrollView dataTable(List<Employee> employees) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.vertical,
+      child: DataTable(
+        columns: [
+          DataColumn(
+            label: Text('NAME'),
+          ),
+          DataColumn(
+            label: Text('DELETE'),
+          )
+        ],
+        rows: employees
+            .map(
+              (employee) => DataRow(cells: [
+                DataCell(
+                  Text(employee.name),
+                  onTap: () {
+                    setState(() {
+                      isUpdating = true;
+                      curUserId = employee.id;
+                    });
+
+                    controller.text = employee.name;
+                  },
+                ),
+                DataCell(IconButton(
+                  icon: Icon(Icons.delete),
+                  onPressed: () {
+                    dbHelper.delete(employee.id);
+                    refreshList();
+                  },
+                ))
+              ]),
+            )
+            .toList(),
+      ),
+    );
+  }
+
+  list() {
+    return Expanded(
+      child: FutureBuilder(
+        future: employees,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return dataTable(snapshot.data);
+          }
+
+          if (null == snapshot.data || snapshot.data.length == 0) {
+            return Text('No data found');
+          }
+
+          return CircularProgressIndicator();
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('sqlite'),
+      ),
+      body: new Container(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          verticalDirection: VerticalDirection.down,
+          children: <Widget>[
+            form(),
+            list(),
+          ],
+        ),
+      ),
+    );
+  }
+}
